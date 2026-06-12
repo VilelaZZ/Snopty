c3c8fd36810819719c4a5f63b189cbaeffbbb76d
Aluno <aluno@fatecitu.local>
2026-06-12 16:33:35 -0300

    [lincon] - Alterações do tailwindcss e algumas coisas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,11 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [mensagem, setMensagem] = useState("");
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/teste/")
-      .then((response) => response.json())
-      .then((data) => setMensagem(data.mensagem))
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
-    <div>
-      <h1>Comunicação React + Django</h1>
-      <p>{mensagem}</p>
-    </div>
+    <div className="">Hello</div>
   );
 }
+
+//ta tudo certinho pra começar a programar
 
 export default App;
